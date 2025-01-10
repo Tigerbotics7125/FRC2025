be7228333a93874922902083d31c0e36fb2d7362
Jeffrey Morris <ffejcsirrom@gmail.com>
2025-01-10 15:38:52 -0600

    Clean out project

---
+++ src/main/java/org/tigerbotics/Robot.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025 Tigerbotics and it's members. All rights reserved.
+ * This work is licensed under the terms of the GNU GPLv3 license
+ * found in the root directory of this project.
+ */
+package org.tigerbotics;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+
+    public Robot() {}
+}
