a645271f6517af9bc9cb091fc174b290ae68fcce
DAflamingFOX <DAflamingFOX@users.noreply.github.com>
2025-01-17 01:44:17 +0000

    [bot] Lint

--- src/main/java/org/tigerbotics/subsystem/Drivetrain.java
+++ src/main/java/org/tigerbotics/subsystem/Drivetrain.java
@@ -17,7 +17,6 @@ import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkClosedLoopController.ArbFFUnits;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.studica.frc.AHRS;
