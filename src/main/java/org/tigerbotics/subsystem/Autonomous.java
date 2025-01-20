af4d8b10ab96173db0180cbcebea7656216d678e
sbeneze <ssbeneze25@gmail.com>
2025-01-20 14:45:41 -0600

    added print command

--- src/main/java/org/tigerbotics/subsystem/Autonomous.java
+++ src/main/java/org/tigerbotics/subsystem/Autonomous.java
@@ -1,10 +1,7 @@
 package org.tigerbotics.subsystem;
 
-import java.io.IOException;
-
-import org.json.simple.parser.ParseException;
-
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.config.ModuleConfig;
 import com.pathplanner.lib.config.PIDConstants;
@@ -14,8 +11,8 @@ import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Autonomous extends SubsystemBase{
@@ -27,7 +24,7 @@ public class Autonomous extends SubsystemBase{
     this.drive=drive;
     this.odom=odom;
     // All other subsystem initialization
-    // ...
+    NamedCommands.registerCommand("print", Commands.print("asdf"));
 
     // Load the RobotConfig from the GUI settings. You should probably
     // store this in your Constants file
@@ -67,6 +64,7 @@ public class Autonomous extends SubsystemBase{
     // pre-loaded auto/path
     return new PathPlannerAuto("Example Auto");
   }
+  
 
 }
     
